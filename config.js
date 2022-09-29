b7be0afbe21d60b10be36f862c131ab7cabb9eab
Foldex <foldex@pm.me>
2022-09-28 22:16:50 -0400

    Config: Update Config

--- config.js
+++ config.js
@@ -1,49 +1,33 @@
 // A very tridactyl-esque config file.
 
-// ---- Functions ----
-mapkey('p', "Open the clipboard's URL in the current tab", function() {
-    Clipboard.read(function(response) {
-        window.location.href = response.data;
-    });
-});
-
-// Jisho Translate on q/Q
-// Messy
-Front.registerInlineQuery({
-    url: function(q) {
-        return `https://jisho.org/search/${q}`;
-    },
-    parseResult: function(res) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(res.text, "text/html");
-        var result = doc.querySelector("#primary>div.exact_block");
-        if (result) {
-            result.querySelectorAll('div>span.furigana').forEach(function(e){
-                br = document.createElement("br");
-                e.appendChild(br);
-            });
-            result.querySelectorAll('h4').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>div.concept_light-status').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>a.light-details_link').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>span.meaning-abstract').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>span.supplemental_info').forEach(function(e){
-                e.outerHTML = "&nbsp;" + e.outerHTML;
-            });
-            var exp = result.innerHTML;
-            return exp;
-        }    }
-});
+// Compatibility Prefix
+const {
+    Clipboard,
+    Front,
+    Hints,
+    Normal,
+    RUNTIME,
+    Visual,
+    aceVimMap,
+    addSearchAlias,
+    cmap,
+    getClickableElements,
+    imap,
+    imapkey,
+    iunmap,
+    map,
+    mapkey,
+    readText,
+    removeSearchAlias,
+    tabOpenLink,
+    unmap,
+    unmapAllExcept,
+    vmapkey,
+    vunmap
+} = api;
 
 // ---- Settings ----
-Hints.characters = 'asdfgyuiopqwertnmzxcvb';
+Hints.setCharacters('asdfgyuiopqwertnmzxcvb');
 
 settings.defaultSearchEngine = 'd';
 settings.hintAlign = 'left';
@@ -54,26 +38,63 @@ settings.scrollStepSize = 200;
 settings.tabsThreshold = 7;
 settings.modeAfterYank = 'Normal';
 
-// Search Engines
-removeSearchAliasX('b', 's');
-removeSearchAliasX('d', 's');
-removeSearchAliasX('g', 's');
-removeSearchAliasX('h', 's');
-removeSearchAliasX('w', 's');
-removeSearchAliasX('y', 's');
-removeSearchAliasX('s', 's');
+// ---- Map -----
+// --- Hints ---
+// Open Multiple Links
+map('<Alt-f>', 'cf');
 
-addSearchAliasX('amz', 'amazon', 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias=aps&field-keywords=', 's');
-addSearchAliasX('au', 'aur', 'https://aur.archlinux.org/packages/?O=0&SeB=nd&K=', 's');
-addSearchAliasX('aw', 'arch wiki', 'https://wiki.archlinux.org/index.php?title=Special:Search&search=', 's');
-addSearchAliasX('d', 'duckduckgo', 'https://duckduckgo.com/?q=', 's');
-addSearchAliasX('gh', 'github', 'https://github.com/search?utf8=✓&q=', 's');
-addSearchAliasX('j', 'jisho', 'http://jisho.org/search/', 's');
-addSearchAliasX('mtg', 'mtg cards', 'https://scryfall.com/search?q=', 's');
-addSearchAliasX('r', 'reddit', 'https://old.reddit.com/r/', 's');
-addSearchAliasX('st', 'steam', 'https://store.steampowered.com/search/?term=', 's');
-addSearchAliasX('w', 'wikipedia', 'https://en.wikipedia.org/wiki/Special:Search/', 's');
-addSearchAliasX('y', 'invidious', 'https://invidious.snopyta.org/search?q=', 's');
+// Yank Link URL
+map('<Alt-y>', 'ya');
+map('<Alt-u>', 'ya');
+
+// Open Hint in new tab
+map('F', 'C');
+
+// --- Nav ---
+// Open Clipboard URL in current tab
+mapkey('p', "Open the clipboard's URL in the current tab", () => { Clipboard.read(function(response) { window.location.href = response.data; }); });
+
+// Open Clipboard URL in new tab
+map('P', 'cc');
+
+// Current tab Omnibar
+map('o', 'go');
+
+// History Back/Forward
+map('H', 'S');
+map('L', 'D');
+
+// Scroll Page Down/Up
+mapkey("<Ctrl-d>", "Scroll down", () => { Normal.scroll("pageDown"); });
+mapkey("<Ctrl-u>", "Scroll down", () => { Normal.scroll("pageUp"); });
+
+// Next/Prev Page
+map('K', '[[');
+map('J', ']]');
+
+// Open Chrome Flags
+mapkey('gF', '#12Open Chrome Flags', () => { tabOpenLink("chrome://flags/"); });
+
+// --- Tabs ---
+// Tab Delete/Undo
+map('D', 'x');
+mapkey('d', '#3Close current tab', () => { RUNTIME("closeTab"); });
+mapkey('u', '#3Restore closed tab', () => { RUNTIME("openLast"); });
+
+// Move Tab Left/Right w/ one press
+map('>', '>>');
+map('<', '<<');
+
+// Tab Next/Prev
+map('<Alt-j>', 'R');
+map('<Alt-k>', 'E');
+
+// --- Misc ---
+// Yank URL w/ one press (disables other yx binds)
+map('y', 'yy');
+
+// Change focused frame
+map('gf', 'w');
 
 // ---- Unmap -----
 // Proxy Stuff
@@ -101,43 +122,25 @@ unmap('og');
 unmap('od');
 unmap('oy');
 
-// ---- Map -----
-// Misc
-map('<Alt-f>', 'cf');
-map('<Alt-y>', 'ya');
-map('P', 'cc');
-map('F', 'C');
-map('gf', 'w');
-map('`', '\'');
-map('o', 'go');
-// History Back/Forward
-map('H', 'S');
-map('L', 'D');
-// Tab Delete/Undo
-map('D', 'x');
-mapkey('d', '#3Close current tab', function() {
-    RUNTIME("closeTab");
-});
-mapkey('u', '#3Restore closed tab', function() {
-    RUNTIME("openLast");
-});
-// Scroll Page Down/Up
-mapkey('<Ctrl-d>', '#9Forward half page', function() { Normal.feedkeys('3j'); });
-mapkey('<Ctrl-u>', '#9Back half page', function() { Normal.feedkeys('3k'); });
-mapkey('<Ctrl-Space>', '#9Page Up', function() { Normal.feedkeys('5k'); });
-// Tab Next/Prev
-map('<Alt-j>', 'R');
-map('<Alt-k>', 'E');
-// Next/Prev Page
-map(']', ']]');
-map('[', '[[');
-mapkey('K', '#1Click on the previous link on current page', previousPage);
-mapkey('J', '#1Click on the next link on current page', nextPage);
-// Move Tab Left/Right
-map('>', '>>');
-map('<', '<<');
+// ---- Search Engines -----
+removeSearchAlias('b', 's');
+removeSearchAlias('d', 's');
+removeSearchAlias('g', 's');
+removeSearchAlias('h', 's');
+removeSearchAlias('w', 's');
+removeSearchAlias('y', 's');
+removeSearchAlias('s', 's');
 
-// Chrome Flags
-mapkey('gF', '#12Open Chrome Flags', function() {
-    tabOpenLink("chrome://flags/");
-});
+addSearchAlias('ama', 'amazon', 'https://www.amazon.com/s?k=', 's');
+addSearchAlias('ap', 'arch pkg', 'https://www.archlinux.org/packages/?sort=&q=', 's');
+addSearchAlias('aur', 'aur', 'https://aur.archlinux.org/packages/?O=0&SeB=nd&K=', 's');
+addSearchAlias('aw', 'arch wiki', 'https://wiki.archlinux.org/index.php?title=Special:Search&search=', 's');
+addSearchAlias('d',  'ddg', 'https://duckduckgo.com/?q=', 's');
+addSearchAlias('dh', 'docker', 'https://hub.docker.com/search?type=image&q=', 's');
+addSearchAlias('fh', 'flathub', 'https://flathub.org/apps/search/', 's');
+addSearchAlias('gh', 'github', 'https://github.com/search?q=', 's');
+addSearchAlias('pdb', 'proton', 'https://www.protondb.com/search?q=', 's');
+addSearchAlias('r', 'reddit', 'https://libreddit.spike.codes/r/', 's');
+addSearchAlias('st', 'steam', 'https://store.steampowered.com/search/?term=', 's');
+addSearchAlias('wiki', 'wikipedia', 'https://en.wikipedia.org/wiki/Special:Search/', 's');
+addSearchAlias('y', 'yt', 'https://invidious.snopyta.org/search?q=', 's');
